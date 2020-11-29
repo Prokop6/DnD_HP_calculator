54c9fca6efa0d6a70bf81482f286408a056a7e35
Marcin Prokop <marcinprokop6@gmail.com>
2020-11-29 04:30:11 +0100

    plot generation added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
+let inputConstitution =13;
+let inputHitdice = 10; 
+let inputLevel = 5;
+let rolledSample = [];
+
 const roll = (dice) => {
-    return Math.floor(
+    return Math.ceil(
         Math.random()*dice);
 };
 
@@ -20,14 +25,13 @@ const estimateHPforLevel = (CON, dice, level) => {
         hitpoints = hitpoints + estimateHPonLevelup(CON, dice)
         i++
     };
-    console.log(hitpoints);
     return hitpoints
 };
 
 const simulateHPforLevel = (CON, dice, level) => {
     let simulatedHitpoints = []
     let j = 0
-    while (j < 1000){
+    while (j < 10000){
         simulatedHitpoints.push(
             estimateHPforLevel(CON, dice, level)
             )
@@ -35,3 +39,11 @@ const simulateHPforLevel = (CON, dice, level) => {
         };
     return simulatedHitpoints
 };
+const generateSample = () => {
+    rolledSample = simulateHPforLevel(inputConstitution,inputHitdice, inputLevel) 
+    document.getElementById("resultPara").innerText = rolledSample
+    Plotly.newPlot("plot", [{x: rolledSample, type: 'histogram'}])
+};
+
+document.getElementById("ButGenerate").onclick = generateSample;
+
